aa48990b23bd0d6f56d9e7e3a0de0dc3dfe840b3
Mischa <mischa@wasmuth.ws>
2024-06-01 00:52:42 +0200

    Code cleanup Move spaceship code out (from plugin to main game)

--- Editor/Editor/SpaceshipEditor.cs
+++
@@ -1,9 +0,0 @@
-using Sleep0.Logic.Game;
-using UnityEditor;
-
-namespace Sleep0.EditorExtensions
-{
-    [CustomEditor(typeof(Spaceship), true)]
-    public class SpaceshipEditor : CustomEditorOverlayBase<Spaceship, SpaceshipOverlay>
-    { }
-}
